5e0660068b34da1a51f8e9715202ad76b1b9845b
Liaqat Paindah <liaqat.paindah@gmail.com>
2025-08-29 23:30:10 +0430

    Project Setup Added

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 import express from 'express'
+import { GetUsers } from './Controllers/userController.js'
 const app = express()
 const Port = 4000
 app.use(express.json())
-app.get('/users',(req, res)=>{
-    res.json(`Hello world`)
-})
+app.get('/users', GetUsers)
 
 app.listen(Port, ()=>{
     console.log("The server is runining...")
