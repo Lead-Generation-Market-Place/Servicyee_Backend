9daded41c4208312f547f1d6ae5125b4af61d5aa
Khalid-Durrani-2024 <khaliddurrani113@gmail.com>
2025-09-23 00:10:54 +0430

    getting wish lists of a specific user done

--- controllers/wishlistsController.js
+++ controllers/wishlistsController.js
@@ -5,8 +5,10 @@ export const addWishList = async (req, res, next) => {
   try {
     const wishlist = req.body;
     const result = await wishlistsService.addWishList(wishlist);
-    res.status(201).json({ 
-      message: "wishlist added", result });
+    res.status(201).json({
+      message: "wishlist added",
+      result,
+    });
   } catch (error) {
     next(error);
   }
@@ -15,17 +17,39 @@ export const addWishList = async (req, res, next) => {
 export const getWishLists = async (req, res, next) => {
   try {
     const result = await wishlistsService.getAllWishLists();
-    res.status(200).json(
-      {
-         message: "Wishlist Retreived Successfully",
-         count:result.length,
-         data:result 
-        
-        },
-
-    
-    );
+    res.status(200).json({
+      message: "Wishlist Retreived Successfully",
+      count: result.length,
+      data: result,
+    });
   } catch (error) {
     next(error);
   }
 };
+
+export const getWishListsWithSubCategory = async (req, res, next) => {
+  try {
+    const result = await wishlistsService.getAllWishListsWithSubCategory();
+    res.status(200).json({
+      message: "All Wish Lists With SubCategory Retreived",
+      count: result.count,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserWishLists=async(req,res,next)=>{
+  try {
+    const userId=req.params.id;
+    const result=await wishlistsService.getAllUserWishLists(userId);
+    res.status(200).json({
+      message: `Retreived WishList of ${userId} `,
+      count:result.count,
+      data:result
+    });
+  } catch (error) {
+    next(error);
+  }
+}
--- routes/wishlistsRoute.js
+++ routes/wishlistsRoute.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { addWishList,getWishLists } from '../controllers/wishlistsController.js';
+import { addWishList,getWishLists,getWishListsWithSubCategory ,getUserWishLists} from '../controllers/wishlistsController.js';
 import {validateBody} from '../middlewares/validate.middleware.js';
 import {createWishlistsSchema} from '../validators/wishlists.validator.js';
 
 
 const router = express.Router();
 
-router.post('/addwishlist',validateBody(createWishlistsSchema),addWishList);
-router.get('/',getWishLists);
+router.get('/',getWishLists); //retreving all wish list including service name and status
+router.post('/addwishlist',validateBody(createWishlistsSchema),addWishList);  //adding a wish list into wishlists collection with refrence user and service
+router.get('/getwishlistswithsubcategory',getWishListsWithSubCategory); //retreiving all wishLists with subcategory
+router.get('/:id',getUserWishLists);
+
 export default router;
