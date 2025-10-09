30b31624cbd755b4f65c4cf5887f12ddc047eb7d
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-10-09 20:24:49 +0430

    category api fixed and necessary fields added

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-const categoryTypes = ['active', 'deactive'];
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   slug: { type: String, required: true, unique: true },
-  status: { type: String, enum: categoryTypes, default: 'active' },
+  is_active: { type: Boolean, default: true },
   description: { type: String },
   category_image_url: { type: String },
+  
+},{ 
+  timestamps: true, 
+  versionKey: false, 
+  collection: 'services'
 });
 
 export default mongoose.model('Category', categorySchema);
