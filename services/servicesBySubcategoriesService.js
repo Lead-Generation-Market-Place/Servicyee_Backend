556883738b4b2c0f384b7a792d6118b09c6e6935
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-10-14 00:55:15 +0430

    subcategory services completed

--- services/servicesBySubcategoriesService.js
+++
@@ -1,6 +0,0 @@
-import servicesModel from "../models/servicesModel.js";
-
-
-export function getAllServicesBySubCategories() {
-    return servicesModel.find().populate('subcategory_id','name').exec();
-}
