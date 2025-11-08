424ea98ed92a0306afac6931185b6be17d063f55
Esmat-Farjad <esmatullah.hashimi2016@gmail.com>
2025-11-08 23:00:07 +0430

    get the professional lead api completed

--- routes/leadRoute.js
+++ routes/leadRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { acceptLead, createLead } from "../controllers/leadController.js";
+import { acceptLead, createLead, getLeadByProfessionalId } from "../controllers/lead/leadController.js";
 
 const router = express.Router();
 
 router.post("/generate", createLead);
 router.put("/accept", acceptLead);
+router.get("/professional-leads/:professionalId",getLeadByProfessionalId);
 
 export default router;
